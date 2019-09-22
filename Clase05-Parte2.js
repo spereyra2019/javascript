047acaf0f95a081dd698435812201644eb47d04b
spereyra2019 <48894811+spereyra2019@users.noreply.github.com>
2019-09-22 19:57:57 -0300

    Clase 05 - Parte2
    Clase 05 - Parte2

---
+++ Clase05-Parte2.js
@@ -0,0 +1,16 @@
+//Ejemplo break 
+
+for (i=0;i<10;i++){ 
+    document.write (i) 
+    escribe = prompt("dime si continuo preguntando...", "si") 
+    if (escribe == "no") 
+       break 
+}
+
+for (i=0;i<=1000;i++){ 
+    document.write(i + "<br>") 
+    if (i==333) 
+       break; 
+}
+
+
